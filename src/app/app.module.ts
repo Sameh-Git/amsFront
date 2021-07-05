04cff225cc66a904141bea8eb286a0a6a41403fa
Sameh-Git <sameh.trabelsii92@gmail.com>
2021-07-05 16:02:07 +0100

    Two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from  '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +18,9 @@ import { PrefixNgPipe } from './myPipes/prefix-ng.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  providerName : String="Samsung";
+  providerEmail: String="samsung@hotmail.com";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  info(){
+    alert(this.providerName + "-"+ this.providerEmail);
   }
 
 }
