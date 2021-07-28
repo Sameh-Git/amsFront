b4526040dcbf18ae2c90b89bc5a842a7fdb1cb66
Sameh-Git <sameh.trabelsii92@gmail.com>
2021-07-28 23:11:48 +0100

    Integration maquette

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  urlProviders :'http://127.0.0.1:1027/providers',
-  urlAuth :'http://127.0.0.1:1027/basicauth',
+  urlProviders :'http://127.0.0.1:8080/providers',
+  urlAuth :'http://127.0.0.1:8080/basicauth',
   
-  urlRegistration: 'http://127.0.0.1:1027/registration'
+  urlRegistration: 'http://127.0.0.1:8080/registration'
 };
 
 /*
